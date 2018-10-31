9b8cd9444bd0203d6b7d7a57fd458eeb5c645181
wjdwndud0114 <wjdwndud0114@yahoo.com>
2018-10-31 01:45:29 -0700

    Init

---
+++ src/main/java/kj/vroom/vroom/VroomApplication.java
@@ -0,0 +1,12 @@
+package kj.vroom.vroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VroomApplication.class, args);
+	}
+}
