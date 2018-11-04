0837c4bdd067e30a853b047682f35d979d2690de
wjdwndud0114 <wjdwndud0114@yahoo.com>
2018-11-04 04:59:09 -0800

    Allow Access-Control-Allow-Origin for testing

--- src/main/java/kj/vroom/vroom/controller/VehicleController.java
+++ src/main/java/kj/vroom/vroom/controller/VehicleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kj.vroom.vroom.domain.Vehicle;
 import kj.vroom.vroom.service.VehicleService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/vehicle")
 public class VehicleController {
